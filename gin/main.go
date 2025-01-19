c9f4c5c1aa8c6f050f252c1f655f8de25ede202e
ilyasbdlzz <ilyaskenway@gmail.com>
2025-01-19 11:15:49 +0700

    Restful API - First push

---
+++ gin/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type album struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Price float64 `json:"price"`
+}
+	var albums = []album {
+		{ID: "1", Title: "Blue", Price: 56.99},
+		{ID: "2", Title: "Blue", Price: 56.99},
+		{ID: "3", Title: "Blue", Price: 56.99},
+	}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+	if error :=c.BindJSON(& newAlbum); error != nil{
+		c.IndentedJSON(http.StatusBadRequest, gin.H {"message" :"Invalid Request" })
+		return
+	}
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumById(c *gin.Context){
+	id := c.Param("id")
+	
+	for _, album := range albums{
+		if album.ID == id {
+			c.IndentedJSON(http.StatusOK, album)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"Message": "Not Found"})
+} 
+func updateAlbumByID(c *gin.Context){
+	id := c.Param("id")
+	var updatedAlbum album
+
+	if error := c.BindJSON(&updatedAlbum) ; error != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H {"message" : "Invalid request"})
+		return
+	}
+	for i, album := range albums {
+		if album.ID == id {
+			// Update the album details
+			albums[i] = updatedAlbum
+			c.IndentedJSON(http.StatusOK, updatedAlbum)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Not Found"})
+}
+
+func deleteAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, album := range albums {
+		if album.ID == id {
+			albums = append(albums[:i], albums[i+1:]...)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "Album deleted"})
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Not Found"})
+}
+
+
+func main() {
+	router := gin.Default()
+
+	router.GET ("/albums", getAlbums)
+	router.POST ("/albums", postAlbums)
+	router.GET ("/albums/:id", getAlbumById)
+	router.PUT ("/albums/:id", updateAlbumByID)
+	router.DELETE ("/albums/:id", deleteAlbumByID)
+
+	router.Run ("localhost:8080")
+}
